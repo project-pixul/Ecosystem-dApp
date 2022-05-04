6d6fd0bd22db89416fc29e6b1e9958c4d1808407
RK03ind <gamitech95@gmail.com>
2022-05-04 23:42:54 +0530

    shellNav to functional

--- src/components/shellNav.tsx
+++ src/components/shellNav.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { BaseComponent, IShellPage } from "./shellInterfaces";
 import { NavHashLink } from "react-router-hash-link";
-import { TFunction, withTranslation, WithTranslation } from "react-i18next";
+import { withTranslation, useTranslation } from "react-i18next";
 
 import { GiHamburgerMenu } from "react-icons/gi";
 
@@ -33,224 +33,203 @@ export type ShellNavState = {
   wallet: Object;
 };
 
-class ShellNav extends BaseComponent<
-  ShellNavProps & WithTranslation,
-  ShellNavState
-> {
-  private collapseRef = React.createRef<any>();
+const ShellNav = (props: ShellNavProps) => {
+  const collapseRef = React.createRef<any>();
+  const [isSideBarOpen, setSideBarOpen] = React.useState<Boolean>(false);
+  const [walletAddr, setWalletAddr] = React.useState<String>("");
+  const [wallet, setWallet] = React.useState<any>();
+  const { t } = useTranslation();
 
-  constructor(props: ShellNavProps & WithTranslation) {
-    super(props);
-    this.state = {
-      isSideBarOpen: false,
-      walletAddr: "",
-      wallet: {},
-    };
-    this.toggleSideBar = this.toggleSideBar.bind(this);
-  }
-
-  toggleMenu = (e: any) => {
-    if (window.innerWidth < 990) this.collapseRef.current.click();
+  const toggleMenu = (e: any) => {
+    if (window.innerWidth < 990) collapseRef.current.click();
   };
 
-  toggleSideBar() {
-    this.setState((prevState) => ({
-      isSideBarOpen: !prevState.isSideBarOpen,
-    }));
-  }
+  const toggleSideBar = () => {
+    setSideBarOpen((prevState) => {
+      return !prevState;
+    });
+  };
 
-  async connectWallet() {
+  const connectWallet = async () => {
     let wallet = new Wallet();
     let result = await wallet.connect();
     if (result === true) {
       alert("wallet is connected");
       localStorage.setItem("walletAddr", wallet.currentAddress);
-      this.setState({ walletAddr: wallet.currentAddress, wallet: wallet });
+      setWalletAddr(wallet.currentAddress);
+      setWallet(wallet);
     }
-  }
+  };
 
-  async disconnectWallet() {
-    const result = await this.state.wallet.disconnect();
+  const disconnectWallet = async () => {
+    const result = await wallet.disconnect();
+
     if (result) {
       alert("wallet is disconnected");
       throw "The wallet connection was cancelled.";
     }
     localStorage.setItem("walletAddr", "");
-    this.setState({ walletAddr: "", wallet: {} });
-  }
 
-  componentDidMount(): void {
+    setWalletAddr("");
+    setWallet({});
+  };
+
+  React.useEffect(() => {
     let temp = localStorage.getItem("walletAddr");
     if (temp) {
-      this.connectWallet();
+      connectWallet();
     }
-  }
+  }, []);
 
-  render() {
-    const pages = this.readProps().pages || [];
-    const t: TFunction<"translation"> = this.readProps().t;
-    const i18n = this.readProps().i18n;
+  const pages1 = props.pages.slice(0, 2);
+  const pages2 = props.pages.slice(2, 7);
 
-    const pages1 = pages.slice(0, 2);
-    const pages2 = pages.slice(2, 7);
+  const menuItemName = {
+    home: t("nav.home"),
+    about: t("nav.about"),
+    farm: t("nav.farm"),
+    xpixul: t("nav.xpixul"),
+    lottery: t("nav.lottery"),
+  };
 
-    const menuItemName = {
-      home: t("nav.home"),
-      about: t("nav.about"),
-      farm: t("nav.farm"),
-      xpixul: t("nav.xpixul"),
-      lottery: t("nav.lottery"),
-    };
+  const mobileNavIcons = {
+    home: <Pixul />,
+    about: <Stake />,
+    farm: <Farm />,
+    xpixul: <XpixulMobile />,
+    lottery: <Docs />,
+  };
 
-    const mobileNavIcons = {
-      home: <Pixul />,
-      about: <Stake />,
-      farm: <Farm />,
-      xpixul: <XpixulMobile />,
-      lottery: <Docs />,
-    };
-
-    const mobileNavExtraItems = {
-      staking: (
-        <NavHashLink to={`xpixul#staking`} activeClassName="active">
-          <div className="mobile-nav-item">
-            {mobileNavIcons["about"]}
-            <span>STAKING</span>
-          </div>
-        </NavHashLink>
-      ),
-      documents: (
-        <a href="https://www.pixul.io/documents" target="_br">
-          <div className="mobile-nav-item">
-            {mobileNavIcons["lottery"]}
-            <span>DOCS</span>
-          </div>
-        </a>
-      ),
-      migrate: (
-        <NavHashLink to={`xpixul#migrate`} activeClassName="active">
-          <div className="mobile-nav-item">
-            {mobileNavIcons["xpixul"]}
-            <span>xPIXUL</span>
-          </div>
-        </NavHashLink>
-      ),
-    };
-
-    return (
-      <>
-        <div className="navigation-wrapper">
-          <div className="logo-wrapper">
-            <a href="/home">
-              <img
-                src="/images/pixellogowebsitewhite.png"
-                className="img-logo"
-                alt="Pixul"
-              />
-            </a>
-            <GiHamburgerMenu
-              className="menu-toggler"
-              onClick={this.toggleSideBar}
-            />
-          </div>
-          <nav id="mainNav">
-            <ul className="navbar-nav">
-              {pages1.map((page) => {
-                const classes = ["nav-item", page.id];
-                const menuMap = {
-                  home: t("nav.home"),
-                  about: t("nav.about"),
-                };
-                const menuName = (menuMap as any)[`${page.id}`];
-                return (
-                  <li key={`${page.id}`}>
-                    <NavLink
-                      to={page.id}
-                      activeClassName="active"
-                      className={classes.join(" ")}
-                      onClick={this.toggleMenu}
-                    >
-                      {menuName}
-                    </NavLink>
-                  </li>
-                );
-              })}
-
-              {pages2.map((page) => {
-                const classes = ["nav-item", page.id];
-                const menuMap = {
-                  farm: t("nav.farm"),
-                  xpixul: t("nav.xpixul"),
-                  lottery: t("nav.lottery"),
-                };
-                const menuName = (menuMap as any)[`${page.id}`];
-                return (
-                  <li key={`${page.id}`}>
-                    <NavLink
-                      to={page.id}
-                      activeClassName="active"
-                      className={classes.join(" ")}
-                      onClick={this.toggleMenu}
-                    >
-                      {menuName}
-                    </NavLink>
-                  </li>
-                );
-              })}
-              <div className="connect-wallet">
-                {this.state.walletAddr ? (
-                  <>
-                    <img
-                      src="https://res.cloudinary.com/rk03/image/upload/v1650441472/ethereum-2296075-1912034_jtsmzt.png"
-                      alt="wallet"
-                    />
-                    <span onClick={() => this.disconnectWallet()}>
-                      {this.state.walletAddr}
-                    </span>
-                  </>
-                ) : (
-                  <span onClick={() => this.connectWallet()}>
-                    Connect Wallet
-                  </span>
-                )}
-              </div>
-            </ul>
-          </nav>
+  const mobileNavExtraItems = {
+    staking: (
+      <NavHashLink to={`xpixul#staking`} activeClassName="active">
+        <div className="mobile-nav-item">
+          {mobileNavIcons["about"]}
+          <span>STAKING</span>
         </div>
+      </NavHashLink>
+    ),
+    documents: (
+      <a href="https://www.pixul.io/documents" target="_br">
+        <div className="mobile-nav-item">
+          {mobileNavIcons["lottery"]}
+          <span>DOCS</span>
+        </div>
+      </a>
+    ),
+    migrate: (
+      <NavHashLink to={`xpixul#migrate`} activeClassName="active">
+        <div className="mobile-nav-item">
+          {mobileNavIcons["xpixul"]}
+          <span>xPIXUL</span>
+        </div>
+      </NavHashLink>
+    ),
+  };
 
-        <nav id="mobileNav">
-          {pages.map((page) => {
-            const menuName = (menuItemName as any)[`${page.id}`];
-            if (page.id === "about") {
-              return mobileNavExtraItems.staking;
-            } else if (page.id === "lottery") {
-              return mobileNavExtraItems.documents;
-            } else if (page.id === "xpixul") {
-              return mobileNavExtraItems.migrate;
-            }
-            return (
-              <NavHashLink
-                to={`${page.id}`}
-                activeClassName="active"
-                onClick={() => window.scrollTo(0, 0)}
-              >
-                <div className="mobile-nav-item">
-                  {mobileNavIcons[`${page.id}`]}
-                  <span>{menuName.toUpperCase()}</span>
-                </div>
-              </NavHashLink>
-            );
-          })}
+  return (
+    <>
+      <div className="navigation-wrapper">
+        <div className="logo-wrapper">
+          <a href="/home">
+            <img
+              src="/images/pixellogowebsitewhite.png"
+              className="img-logo"
+              alt="Pixul"
+            />
+          </a>
+          <GiHamburgerMenu className="menu-toggler" onClick={toggleSideBar} />
+        </div>
+        <nav id="mainNav">
+          <ul className="navbar-nav">
+            {pages1.map((page) => {
+              const classes = ["nav-item", page.id];
+              const menuMap = {
+                home: t("nav.home"),
+                about: t("nav.about"),
+              };
+              const menuName = (menuMap as any)[`${page.id}`];
+              return (
+                <li key={`${page.id}`}>
+                  <NavLink
+                    to={page.id}
+                    activeClassName="active"
+                    className={classes.join(" ")}
+                    onClick={toggleMenu}
+                  >
+                    {menuName}
+                  </NavLink>
+                </li>
+              );
+            })}
+
+            {pages2.map((page) => {
+              const classes = ["nav-item", page.id];
+              const menuMap = {
+                farm: t("nav.farm"),
+                xpixul: t("nav.xpixul"),
+                lottery: t("nav.lottery"),
+              };
+              const menuName = (menuMap as any)[`${page.id}`];
+              return (
+                <li key={`${page.id}`}>
+                  <NavLink
+                    to={page.id}
+                    activeClassName="active"
+                    className={classes.join(" ")}
+                    onClick={toggleMenu}
+                  >
+                    {menuName}
+                  </NavLink>
+                </li>
+              );
+            })}
+            <div className="connect-wallet">
+              {walletAddr ? (
+                <>
+                  <img
+                    src="https://res.cloudinary.com/rk03/image/upload/v1650441472/ethereum-2296075-1912034_jtsmzt.png"
+                    alt="wallet"
+                  />
+                  <span onClick={() => disconnectWallet()}>{walletAddr}</span>
+                </>
+              ) : (
+                <span onClick={() => connectWallet()}>Connect Wallet</span>
+              )}
+            </div>
+          </ul>
         </nav>
-        {this.state.isSideBarOpen && (
-          <Sidebar
-            sidebarItems={sideBarItems}
-            toggleMenu={this.toggleSideBar}
-          />
-        )}
-      </>
-    );
-  }
-}
+      </div>
+
+      <nav id="mobileNav">
+        {props.pages.map((page) => {
+          const menuName = (menuItemName as any)[`${page.id}`];
+          if (page.id === "about") {
+            return mobileNavExtraItems.staking;
+          } else if (page.id === "lottery") {
+            return mobileNavExtraItems.documents;
+          } else if (page.id === "xpixul") {
+            return mobileNavExtraItems.migrate;
+          }
+          return (
+            <NavHashLink
+              to={`${page.id}`}
+              activeClassName="active"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              <div className="mobile-nav-item">
+                {mobileNavIcons[`${page.id}`]}
+                <span>{menuName.toUpperCase()}</span>
+              </div>
+            </NavHashLink>
+          );
+        })}
+      </nav>
+      {isSideBarOpen && (
+        <Sidebar sidebarItems={sideBarItems} toggleMenu={toggleSideBar} />
+      )}
+    </>
+  );
+};
 
 export default withTranslation()(ShellNav);
